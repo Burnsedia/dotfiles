93c8abb90bfe58b829bdb14715101da72c378280
Burnsedia <bburnsed1995@gmail.com>
2025-07-04 00:00:52 -0400

    updated my autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 function run {
-  if ! pgrep $1 ;
-  then
-    $@&
+  if ! pgrep $1; then
+    $@ &
   fi
 }
-run dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
 #run xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
 #run xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 #autorandr horizontal
